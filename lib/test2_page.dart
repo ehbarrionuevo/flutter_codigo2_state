88d3b53aa54cc7f9c087840d5b4cbd06912f7b7a
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-23 22:36:44 -0500

    Test2 2

--- lib/test2_page.dart
+++ lib/test2_page.dart
@@ -8,6 +8,8 @@ class Test2Page extends StatefulWidget {
 }
 
 class _Test2PageState extends State<Test2Page> {
+  bool isTitleBold = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +24,7 @@ class _Test2PageState extends State<Test2Page> {
               "Justice League",
               style: TextStyle(
                 fontSize: 20.0,
+                fontWeight: isTitleBold ? FontWeight.bold : FontWeight.normal,
               ),
             ),
             SizedBox(
@@ -29,6 +32,22 @@ class _Test2PageState extends State<Test2Page> {
             ),
             Text(
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ),
+            const Divider(),
+            // Checkbox(
+            //   value: isTitleBold,
+            //   onChanged: (bool? value) {
+            //     isTitleBold = value!;
+            //     setState(() {});
+            //   },
+            // ),
+            CheckboxListTile(
+              title: Text("Title bold"),
+              value: isTitleBold,
+              onChanged: (bool? value) {
+                isTitleBold = value!;
+                setState(() {});
+              },
             ),
           ],
         ),
