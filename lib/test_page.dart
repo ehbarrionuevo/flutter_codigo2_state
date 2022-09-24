1a536744b50c970a58aa865b51f273a6f2ce0701
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-23 21:21:44 -0500

    Test Page 3

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,8 +1,15 @@
-
 import 'package:flutter/material.dart';
 
 class TestPage extends StatelessWidget {
-  const TestPage({Key? key}) : super(key: key);
+
+  List<String> images = [
+    "https://images.pexels.com/photos/13054203/pexels-photo-13054203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/13684853/pexels-photo-13684853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/13252401/pexels-photo-13252401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1  ",
+    "https://images.pexels.com/photos/13522034/pexels-photo-13522034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/13496827/pexels-photo-13496827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +19,29 @@ class TestPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          //Image.network("https://images.pexels.com/photos/13054203/pexels-photo-13054203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",),
-          Image.asset("assets/images/image1.jpeg"),
+          Image.network(
+            "https://images.pexels.com/photos/13054203/pexels-photo-13054203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            width: double.infinity,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Anterior",
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Siguiente",
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
