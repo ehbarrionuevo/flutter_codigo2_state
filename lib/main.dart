103dcddd4b0d5cdac2de40ac592411fed6b5d379
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-23 20:19:08 -0500

    Test Page 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:codigo2_state/home_page.dart';
+import 'package:codigo2_state/test_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,68 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: TestPage(),
     );
   }
 }
 
 
 
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage> {
-
-  int contador = 100;
-
-  List<String> names = [
-    "Juan",
-    "Luis",
-    "Carlos",
-    "Dario",
-    "Fiorella",
-    "Manuel",
-    "Diego",
-  ];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hola a todos"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              contador.toString(),
-              style: TextStyle(
-                fontSize: 40.0
-              ),
-            ),
-            SizedBox(
-              height: 6.0,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                contador = contador + 1;
-                print(contador);
-                setState(() {});
-              },
-              child: Text(
-                "Siguiente",
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 
 
